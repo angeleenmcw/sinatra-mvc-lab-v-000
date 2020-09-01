bbf91f80a6b815eceb6d183b9d3fecf998d74c75
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-01 07:13:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+
   get '/' do
     erb :user_input
   end
-  
+
   post '/piglatinize' do
     @text = params[:user_phrase]
-    @piglatinizer = Piglatinizer.new
-    erb :Piglatinizer
+    @piglatinizer = PigLatinizer.new
+    erb :piglatinize
   end
-end
